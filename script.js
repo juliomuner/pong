c229c1956f2bba1acf57902121e3b1363c133f93
juliomuner <juliomuner@gmail.com>
2022-02-01 15:01:02 -0300

    Inteligência artificial simples

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let raio = 10
 let velocidadeXBolinha = 10
 let fatorX = +1
 let fatorY = -1
-let ataque = 4
+let ataque = 3
 
 let tela = document.querySelector('canvas')
 let pincel = tela.getContext('2d')
@@ -43,6 +43,16 @@ function desenhaRaqueteA(y) {
     pincel.fillRect (10,y,5,80)
 }
 
+// CRIANDO RAQUETE AUTOMATICA B
+function desenhaRaqueteB(y) {
+    if (y > 100 && y <= 440) {
+        pincel.fillStyle = 'black'  // limpar rastro da raquete
+        pincel.fillRect(590,0,15,400) // limpar rastro da raquete
+        pincel.fillStyle = 'white'
+        pincel.fillRect (590,y-(25*ataque),5,80)  // inteligencia artificial
+    }
+    }
+
 // MOVENDO COM SETAS
 var cima = 38;
 var baixo = 40;
@@ -67,6 +77,7 @@ setInterval(refresh, velocidadeXBolinha)
 function refresh() {
     limpaRastroBolinha(xBolinha,yBolinha)
     desenhaRaqueteA(yRaqueteA)
+    desenhaRaqueteB(yBolinha)
     desenhaBolinha(xBolinha,yBolinha)
     colide()
     xBolinha = xBolinha + fatorX * ataque
