2db3051ea1773b04df78000be12d9a5e2d5bd5a2
juliomuner <juliomuner@gmail.com>
2022-02-01 13:50:50 -0300

    Alterando raio como variável

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let xBolinha = 300
 let yBolinha = 200
+let raio = 10
 let velocidadeXBolinha = 10
 
 let tela = document.querySelector('canvas')
@@ -10,14 +11,14 @@ pincel.fillRect(0,0,600,400)
 function desenhaBolinha(x,y) {
     pincel.fillStyle = 'white'
     pincel.beginPath()
-    pincel.arc(x,y,15,0,Math.PI*2)
+    pincel.arc(x,y,raio,0,Math.PI*2)
     pincel.fill()
 }
 
 function limpaRastroBolinha(x,y){
     pincel.fillStyle = 'black'
     pincel.beginPath()
-    pincel.arc(x-1,y,20,0,Math.PI*2)
+    pincel.arc(x-1,y,raio+5,0,Math.PI*2)
     pincel.fill()
 }
 
@@ -27,6 +28,9 @@ function moveBolinha() {
 
     xBolinha++
     
+}
+
+function colisao() {
 }
 
 setInterval(moveBolinha, velocidadeXBolinha)
