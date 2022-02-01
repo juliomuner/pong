de61b33f67604a04c70e9603f90b3e7bb3a59414
juliomuner <juliomuner@gmail.com>
2022-02-01 14:00:29 -0300

    Bolinha quicando na caixa

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ let xBolinha = 300
 let yBolinha = 200
 let raio = 10
 let velocidadeXBolinha = 10
+let fatorX = +1
+let fatorY = -1
 
 let tela = document.querySelector('canvas')
 let pincel = tela.getContext('2d')
@@ -25,12 +27,24 @@ function limpaRastroBolinha(x,y){
 function moveBolinha() {
     limpaRastroBolinha(xBolinha,yBolinha)
     desenhaBolinha(xBolinha,yBolinha)
-
-    xBolinha++
+    colide()
+    xBolinha = xBolinha + fatorX 
+    yBolinha = yBolinha + fatorY
     
 }
 
-function colisao() {
+function colide() {
+    if (xBolinha === 600-raio) {
+        fatorX = -1
+    } else if (xBolinha === raio) {
+        fatorX = +1
+    }
+    if (yBolinha === 400-raio) {
+        fatorY = -1
+    } else if (yBolinha === raio) {
+        fatorY = +1
+    }
 }
+colide
 
 setInterval(moveBolinha, velocidadeXBolinha)
