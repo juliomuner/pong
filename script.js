879c056da9467e26d5219a0f9c943ee2fa99a4e6
juliomuner <juliomuner@gmail.com>
2022-02-01 23:54:00 -0300

    Muitas mudanças

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 let xBolinha = 300
 let yBolinha = 200
 let raio = 10
-let velocidadeXBolinha = 10
-let fatorX = +1
+let velocidadeXBolinha = 15
+let fatorX = 1
 let fatorY = -1
-let ataque = 3
+let ataque = 0
+let yRaqueteA = 10
 
 let tela = document.querySelector('canvas')
 let pincel = tela.getContext('2d')
@@ -27,35 +28,56 @@ function limpaRastroBolinha(x,y){
     pincel.fill()
 }
 let vida = 4
-function colide() {
+function colisao() {
+    let contato = localDaBatida(yBolinha, yRaqueteA)
     let colideA = (yBolinha >= yRaqueteA && yBolinha <= yRaqueteA+80 && xBolinha <= 15+raio) ? true : false
-    let colideB = true
-    if (xBolinha >= 600-raio) {
+    let colideB = false 
+
+    if (xBolinha >= 600-raio || colideB) {
         fatorX = -1
         if (colideB == false) {
-            vida++
+            vida = vida + (vida < 9 ? 1 : 0)
             footer.innerHTML = vida+1 // só pra ve o html
             desenhaPixel(vida,'blue')
         }
     } else if (xBolinha <= raio || colideA) {
-        fatorX = +1
-        if (colideA == false) {
-            vida--
-            desenhaPixel(vida,'red')
-            footer.innerHTML = vida+1 // só pra ve o html
+        fatorX = +1 // tanto faz a colisão, altera a o sentido
+        ataque = contato // em qualquer colisão muda o angulo de ataque conforme função localDaBatida
+        
+        // se a colisão for na casquinha
+        if (contato == 5 && yBolinha < yRaqueteA +40) {
+            fatorY = -1
+        } else if (contato == 5 && yBolinha > yRaqueteA +40) {
+            fatorY = +1
         }
+
+        if (colideA == false) {     // porém se não bater na raquete
+            vida = vida + (vida >= 0 ? -1 : 0)
+            desenhaPixel(vida,'red')
+            ataque = 2
+        }
+        
     }
 
+    // Colisão com topo ou chao
     if (yBolinha >= 400-raio) {
         fatorY = -1
     } else if (yBolinha <= raio) {
         fatorY = +1
     }
+    
 }
 
 function desenhaRaqueteA(y) {
+    pincel.fillStyle = 'black'  // limpar rastro da raquete
+    pincel.fillRect(0,0,15,400) // limpar rastro da raquete
     pincel.fillStyle = 'white'
-    pincel.fillRect (10,y,5,80)
+    let y2 = y
+    if (y < 10) {
+        y2 = 10
+    } else if (y > 310) {
+        y2 = 310}
+    pincel.fillRect (10,y2,5,80)
 }
 
 // RAQUETE-B AUTOMATICA
@@ -73,32 +95,39 @@ function desenhaRaqueteB(y) {
 var cima = 38;
 var baixo = 40;
 
-let yRaqueteA = 10
-function leDoTeclado(evento) { 
-    if (evento.keyCode == baixo && yRaqueteA <= 300) {
-        yRaqueteA=yRaqueteA+30
-    } else if (evento.keyCode == cima && yRaqueteA >= 20) {
-        yRaqueteA=yRaqueteA-30
-    }
-    pincel.fillStyle = 'black'  // limpar rastro da raquete
-    pincel.fillRect(0,0,15,400) // limpar rastro da raquete
+
+let vel = 1
+function teclado(evento) { 
+    if (evento.keyCode == baixo) {
+        vel++
+    } else if (evento.keyCode == cima) {
+        vel--
+    }  
+}
+document.onkeydown = teclado
+// MOVENDO COM SETAS */
+
+//movendo com mouse
+function movimentaRaquete(evento) {
+    yRaqueteA = evento.pageY - tela.offsetTop -40
     desenhaRaqueteA(yRaqueteA)
 }
-document.onkeydown = leDoTeclado
-// MOVENDO COM SETAS
+tela.onmousemove = movimentaRaquete
 
 // REFRESH
 setInterval(refresh, velocidadeXBolinha)
-
 function refresh() {
     limpaRastroBolinha(xBolinha,yBolinha)
+    desenhaBolinha(xBolinha,yBolinha)
     desenhaRaqueteA(yRaqueteA)
     desenhaRaqueteB(yBolinha)
-    desenhaBolinha(xBolinha,yBolinha)
-    colide()
+
+    colisao()
     pontua()
-    xBolinha = xBolinha + fatorX * ataque
-    yBolinha = yBolinha + fatorY * ataque
+    xBolinha = xBolinha + fatorX*(5+vel)
+    yBolinha = yBolinha + fatorY * ataque 
+    footer.innerHTML = `xBolinha: ${xBolinha}<br>yBolinha: ${yBolinha}<br>yRaqueteA:${yRaqueteA}
+    <br>ataque: ${ataque}<br> velocidade bolinha${vel}`
     
 }
 // REFRESH
@@ -114,8 +143,6 @@ function pontua(){
         desenhaPixel(x,cor)
     } 
 }
-
-
 function desenhaPixel(x,cor) {
     pincel.fillStyle = cor
     pincel.fillStroke = 'black'
@@ -125,3 +152,17 @@ function desenhaPixel(x,cor) {
 // SISTEMA DE PONTOS
 
 var footer = document.querySelector('footer') // só para ver o codigo funcionante
+
+
+ let localDaBatida = function(x,y) {
+    if (x >= y+40 && x <= y+60) {
+        local = 2 // centro (y+40 a y+60)
+    } else if ((x >= y+10 && x < y+40) || (x > y+60 && x <= y+70)) {
+        local = 3 // halo (y+10 a y + 40) (y+40 a y+70)
+    } else if ((x >= y && x < y+10) || (x > y+70 && x <= y+80)) {
+        local = 5 // canto (y a y+10) (y+70 a y+80)
+    } else {
+        local = 0 // fora
+    }
+    return Number(local)
+}
